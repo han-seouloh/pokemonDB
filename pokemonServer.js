9535154eaed6d9977ce603f8ef501b96d63758bc
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-03-09 19:21:18 -0600

    Add provisional text

--- pokemonServer.js
+++ pokemonServer.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 4000;
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World');
+  res.send(`
+  <h1>Pokemon Server</h1>
+  <p>This server is working!</p>
+  <p>But it's still currently under construction...</p>
+  `);
 });
 
 app.listen(PORT, () => {
