2cb8cd48f70f46d274629bb04bb19e4476c6913f
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-12-04 00:02:56 -0600

    Test changes.

--- pokemonServer.js
+++ pokemonServer.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.get('/', (req, res, next) => {
-  res.send(`
-  <h1>Pokemon Server</h1>
-  <p>This server is working!</p>
-  <p>But it's still currently under construction...</p>
-  `);
+const data = require('./data.json');
+
+app.get('/pokemon-api/', (req, res, next) => {
+  res.send(data);
 });
 
 app.listen(PORT, () => {
