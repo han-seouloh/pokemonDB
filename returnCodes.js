2b8687dc32fe2e316397d848594c26620e887ef8
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-12-05 14:12:38 -0600

    Create return codes utility

---
+++ returnCodes.js
@@ -0,0 +1,14 @@
+const ReturnCodes = {
+  // GENERAL RETURN CODES
+  SUCCESS: 0,
+  GENERAL_FAILURE: 1,
+
+  // REQ. BODY ERROR
+  NOT_A_NUMBER: 10,
+  NOT_A_STRING: 11,
+  NOT_AN_ARRAY: 12
+}
+
+module.exports = {
+  ReturnCodes
+}
