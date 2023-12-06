108d11f40eb1fb008e85c9971142d47b6a586962
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-12-05 22:09:49 -0600

    Add req query return codes

--- returnCodes.js
+++ returnCodes.js
@@ -3,10 +3,16 @@ const ReturnCodes = {
   SUCCESS: 0,
   GENERAL_FAILURE: 1,
 
-  // REQ. BODY ERROR
+  // REQ. BODY CODES
   NOT_A_NUMBER: 10,
   NOT_A_STRING: 11,
-  NOT_AN_ARRAY: 12
+  NOT_AN_ARRAY: 12,
+
+  // REQ. QUERY CODEs
+  Q_INVALID: 20,
+  Q_EMPTY: 21,
+  Q_NAME: 22,
+  Q_TYPE: 23,
 }
 
 module.exports = {
