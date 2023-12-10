55123bbe4d160ef6ee234b475640f432dc22441c
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-12-09 19:24:15 -0600

    Refactor error handling

--- pokedexRouter.js
+++ pokedexRouter.js
@@ -1,34 +1,12 @@
 const express = require('express');
 const pokedexRouter = express.Router();
 
-const { findIndexById, verifyEntry, checkQuery, filterByName, filterByType, checkID } = require('./util.js');
+const { findIndexById, checkQuery, filterByName, filterByType, checkID, createError, validateEntry } = require('./util.js');
 const { ReturnCodes } = require('./returnCodes.js');
 
 const data = require('./db/pokemonData.json');
 
-
-// ============= MIDDLEWARE FUNCTIONS =============
-const validateEntry = (req, res, next) => {
-  const retCode = verifyEntry(req.body.entry);
-
-  switch (retCode) {
-    case ReturnCodes.SUCCESS:
-      return next();
-
-    case ReturnCodes.GENERAL_FAILURE:
-      return res.send('Missing entry properties or invalid id.');
-
-    case ReturnCodes.NOT_A_NUMBER:
-      return res.send('id property is not a number.');
-
-    case ReturnCodes.NOT_A_STRING:
-      return res.send('name, description or type array elements are not strings.');
-
-    case ReturnCodes.NOT_AN_ARRAY:
-      return res.send('type property is not an array.')
-  }
-}
-
+// ================ MIDDLEWARE =================
 pokedexRouter.param('id', (req, res, next, id) => {
   if (id === 'all') {
     req.index = id;
@@ -38,8 +16,11 @@ pokedexRouter.param('id', (req, res, next, id) => {
   if (index !== -1) {
     req.index = index;
     next();
+
   } else {
-    res.status(404).send('Pokemon Entry Not Found');
+    const err = createError(204, 'Pokemon Entry Not Found');
+    return next(err);
+    
   }
 });
 
@@ -53,7 +34,8 @@ pokedexRouter.get('/:id', (req, res, next) => {
         return res.status(200).send(data);
 
       case ReturnCodes.Q_INVALID:
-        return res.send('Invalid query parameter.');
+        const err = createError(400, 'Invalid query parameter.');
+        return next(err);
 
       case ReturnCodes.Q_NAME:
         const entriesByName = filterByName(req.query.name, data);
@@ -79,7 +61,9 @@ pokedexRouter.post('/', validateEntry, (req, res, next) => {
     data.push(entry);
     return res.status(200).send(entry);
   } else {
-    res.send('This entry id already exists.');
+    const err = createError(400, 'This entry id already exists.');
+    return next(err);
+
   }
 });
 
@@ -91,28 +75,35 @@ pokedexRouter.put('/', validateEntry, (req, res, next) => {
     case ReturnCodes.VALID_ID:
       const index = findIndexById(Number(entry.id), data);
       if (index === -1) {
-        return res.status(404).send(`Could not find entry to update with ID:${entry.id}`);
+        const err = createError(204, `Could not find entry to update with ID:${entry.id}`);
+        return next(err);
+
       } else {
         data[index] = entry;
         return res.status(200).send({status: 'SUCCESS', updatedEntry: entry});
       }
 
     case ReturnCodes.INVALID_ID:
-      return res.send('This is an invalid ID. You cannot remove any of the original 151 pokemon.')
+      const err = createError(304, 'You cannot modify any of the original 151 pokemon entries.');
+      return next(err);
   }
 });
 
 // ==================== DELETE ====================
 pokedexRouter.delete('/:id', (req, res, next) => {
   if (req.index < 151) {
-    res.send('Error: You cannot delete any of the original 151 pokemon entries.');
+    const err = createError(403, 'You cannot delete any of the original 151 pokemon entries.');
+    next(err);
+
+  } else if (req.index === 'all') {
+    const err = createError(400, 'Bad Request. Specify ID.');
+    next(err);
+
   } else {
     const deletedEntry = data.splice(req.index, 1);
     res.send({status:'SUCCESS', deletedEntry: deletedEntry});
+
   }
 });
-
-// ================ ERROR HANDLERS ================
-// TODO - ErrorHandler
 
 module.exports = pokedexRouter;
