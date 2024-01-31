df34e8d7332bdec5ffe406263b92b9658a9bfb40
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2024-01-31 00:10:05 -0600

    Finish loginRouter tests

--- test/1-loginRouter.test.js
+++ test/1-loginRouter.test.js
@@ -4,6 +4,8 @@ const app = require('../pokemonServer');
 require('dotenv').config();
 
 describe('Testing loginRouter routes...', () => {
+  const agent = request.agent(app);
+
   describe('Testing /register route...', () => {
     describe('User is registering...', () => {
       it('Registering prof_birch...', async () => {
@@ -35,6 +37,18 @@ describe('Testing loginRouter routes...', () => {
 
         assert.equal(result, 2);
       });
+
+      it('Verifying duplicate registers should return an error...', async () => {
+        const username = 'prof_birch';
+        const password = 'Pokedex2*';
+        
+        const response = await request(app)
+          .post('/register')
+          .send({username, password});
+        const data = JSON.parse(response.text);
+
+        assert.include(data.error, 'Username already exists');
+      });
     });
   });
 
@@ -60,25 +74,31 @@ describe('Testing loginRouter routes...', () => {
         assert.include(response.text, 'Redirecting to /login');
       });
 
-      it('Verifying successful login', async () => {
-        const response = await request(app)
+      it('Verifying successful login', (done) => {
+        agent
           .post('/login')
           .send({
             username: process.env.ADMIN_USERNAME,
             password: process.env.ADMIN_PASSWORD
           })
-        
-          assert.include(response.text, 'Redirecting to /');
+          .end((err, res) => {
+            if (err) throw err;
+            assert.equal(res.text, 'Found. Redirecting to /');
+            done();
+          });
       });
     });
   });
 
   describe('Testing /logout route...', () => {
-    it('User successfully logs out...', async () => {
-      const response = await request(app)
-        .get('/logout');
-
-      assert.include(response.text, 'Redirecting to /login');
+    it('User successfully logs out...', (done) => {
+      agent
+        .get('/logout')
+        .end((err, res) => {
+          if (err) throw err; 
+          assert.include(res.text, 'Redirecting to /login');
+          done();
+        });
     });
   });
 });
